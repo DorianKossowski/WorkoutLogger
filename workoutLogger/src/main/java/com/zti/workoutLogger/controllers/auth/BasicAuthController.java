6e3ce9d4c510edef40a063f8828b66abd71e30ed
dorian.kossowski <dorian.kossowski@compactbi.com>
2020-04-09 19:51:36 +0200

    Add logging with slf4j

--- workoutLogger/src/main/java/com/zti/workoutLogger/controllers/auth/BasicAuthController.java
+++ workoutLogger/src/main/java/com/zti/workoutLogger/controllers/auth/BasicAuthController.java
@@ -1,9 +1,10 @@
 package com.zti.workoutLogger.controllers.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +16,19 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @CrossOrigin(origins = "*")
 public class BasicAuthController {
+    private static final Logger logger = LoggerFactory.getLogger(BasicAuthController.class);
 
     @GetMapping("/basicLogin")
     public void login() {
+        logger.debug("User logged in");
     }
 
     @PostMapping("/basicLogout")
     public void logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
+        if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
+            logger.debug("User logged out");
         }
     }
 }
